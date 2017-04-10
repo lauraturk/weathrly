151614ed4fa8ffad78bd8bf9d594b2fca96e48ff
John Binning <john.binning17@gmail.com>
2017-04-10 16:39:20 -0600

    add input class, hook up to dom

---
+++ lib/Input.jsx
@@ -0,0 +1,13 @@
+const ReactDOM = require('react-dom');
+import React, { Component } from 'react';
+
+export default class Input extends Component {
+  render() {
+    return (
+      <button>
+        I Have Been Clicked 0 Times
+      </button>
+    );
+  }
+
+}
