7b1be0623a2faa511230c092cb1f25cf4c56ba89
John Binning <john.binning17@gmail.com>
2017-04-10 16:59:33 -0600

    add input field with input for city

--- lib/Input.jsx
+++ lib/Input.jsx
@@ -1,12 +1,14 @@
-const ReactDOM = require('react-dom');
 import React, { Component } from 'react';
 
 export default class Input extends Component {
   render() {
     return (
-      <button>
-        I Have Been Clicked 0 Times
-      </button>
+      <div>
+        <input placeholder="Select a City"></input>
+        <button >
+          Save
+        </button>
+      </div>
     );
   }
 
