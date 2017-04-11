f8ad92e4c381c1b841ea689fdba2957f06b43405
John Binning <john.binning17@gmail.com>
2017-04-10 19:22:50 -0600

    use ajax to fetch data

--- lib/App.jsx
+++ lib/App.jsx
@@ -1,22 +1,57 @@
 import React, {Component} from 'react'
 import Current from './Current'
+import keys from '../keys'
 
 export default class App extends Component {
   constructor() {
     super()
+    this.url = `http://api.wunderground.com/api/${keys.johnKey}/forecast/q/CO/Denver.json`
     this.state = {
-      temp: '-23',
-      date: new Date(),
-      location: 'Denver'
+      weatherFetched: false
     };
   }
 
+  componentDidMount() {
+    $.ajax({
+      method: 'GET',
+      url: this.url,
+      success: (data) => {
+        const state = Object.assign(
+          {},
+          { weatherFetched: true },
+          data.forecast.simpleforecast.forecastday[0]
+        )
+        this.setState(state);
+      }
+    });
+  }
+
+  renderCurrent() {
+    const {
+      date,
+      high,
+      low,
+      conditions,
+      icon_url,
+    } = this.state;
+
+    return (
+      <Current
+        date={date}
+        high={high}
+        low={low}
+        conditions={conditions}
+        icon_url={icon_url}
+      />
+    );
+  }
+
   render() {
-    const { temp, date } = this.state;
+    const current = this.state.weatherFetched ? this.renderCurrent() : null;
     return (
       <div>
         <h1>Weatherly</h1>
-        <Current temp={temp} date={date} />
+        {current}
       </div>
     )
   }
--- lib/Current.jsx
+++ lib/Current.jsx
@@ -4,12 +4,23 @@ import moment from 'moment'
 export default class Current extends Component {
 
   render() {
-    const { date, temp } = this.props;
-    const formattedDate = moment(date).format('dddd MMM Do');
+    const {
+      date,
+      high,
+      low,
+      conditions,
+      icon_url,
+    } = this.props;
+
     return (
       <div>
-        <h4>The current tempature is: {temp} degrees.</h4>
-        <h4>The current time is: {formattedDate}</h4>
+        <h3>{date.weekday}</h3>
+        <img src={icon_url} />
+        <h4>{conditions}</h4>
+        <ul>
+          <li><strong>High: </strong> {high.fahrenheit}</li>
+          <li><strong>Low: </strong> {low.fahrenheit}</li>
+        </ul>
       </div>
     );
   }
