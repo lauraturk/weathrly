9cdd2edd4da5dfcfa9cd779235c75d36d15e5aac
Laura Turk <laura.lk.turk@gmail.com>
2017-04-11 14:44:37 -0600

    Create App and connect to index.js

---
+++ lib/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <nav id="input">
+      input here
+      </nav>
+    )
+  }
+}
