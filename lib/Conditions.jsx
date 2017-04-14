297f85d196c62d2e3ca0605717ff9592e3c68e39
John Binning <john.binning17@gmail.com>
2017-04-14 02:03:51 -0600

    update condition-style to be on a .then

--- lib/Conditions.jsx
+++ lib/Conditions.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import conditionStyles from './conditionStyles'
 
+/* <div className='hidden'>{conditionStyles(conditions )}</div> */
+/* <div>{console.log(conditions, ' trial')}</div> */
 
 export default function Conditions ({conditions})  {
   return (
     <div className="conditions">
-      <div>{console.log(conditions, ' trial')}</div>
       <div className="condition-location">{conditions.location}</div>
-      <div className='hidden'>{conditionStyles(conditions )}</div>
       <div className="condition-temp-f">{conditions.tempF}</div>
       <img className="condition-img"src={conditions.icon}/>
       <div className="condition-status">{conditions.status}</div>
