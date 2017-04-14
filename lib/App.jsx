caa2f92458c717f3c4973e5a329bd95cbc6a8b73
Laura Turk <laura.lk.turk@gmail.com>
2017-04-13 20:57:21 -0600

    Break out scrubber

--- lib/App.jsx
+++ lib/App.jsx
@@ -3,9 +3,10 @@ import Input from './Input'
 import TenDay from './TenDay'
 import Conditions from './Conditions'
 // import DataSet from './api.js'
-import Scrubber from './Scrubber'
+// import Scrubber from './Scrubber'
 import keys from '../keys'
 import * as $ from 'jquery'
+import scrubData from './scrubData'
 
 
 export default class App extends Component {
@@ -30,7 +31,7 @@ export default class App extends Component {
     var url = `http://api.wunderground.com/api/${keys.johnKey}/conditions/hourly/forecast10day/q/${input}.json`
 
     $.get(url).then( (dataResponse) => {
-      this.scrubData(dataResponse)
+      this.setState(scrubData(dataResponse))
     })
     this.setState({
       url,
@@ -38,47 +39,7 @@ export default class App extends Component {
     })
   }
 
-  scrubData (data) {
-    this.setState({
-      tenDayWeather: this.scrubDataTenDay(data),
-      conditions: this.scrubDataConditions(data),
-      hourly: this.scrubDataHourly(data)
-    })
-  }
 
-  scrubDataConditions (data) {
-    return {
-      status: data.current_observation.weather,
-      location: data.current_observation.display_location.full,
-      tempF: data.current_observation.temp_f,
-      tempC: data.current_observation.temp_c,
-      icon: data.current_observation.icon_url
-    }
-  }
-
-  scrubDataHourly (data) {
-    return data.hourly_forecast.map((hour) => {
-      return {
-        time: hour.FCTTIME.civil,
-        tempE: hour.temp.english,
-        tempM: hour.temp.metric,
-        condition: hour.condition,
-        icon: hour.icon_url
-      }
-    })
-  }
-
-  scrubDataTenDay(data) {
-    return data.forecast.simpleforecast.forecastday.map( ( day ) => {
-      return {
-        date: day.date.pretty,
-        high: day.high.fahrenheit,
-        low: day.low.fahrenheit,
-        condition: day.conditions,
-        icon: day.icon_url
-      }
-    })
-  }
 
   render() {
     return (
