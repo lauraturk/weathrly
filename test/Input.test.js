9403b00979111782d6c2c5b65ae7f23cf3b7a33f
Laura Turk <laura.lk.turk@gmail.com>
2017-04-16 18:05:24 -0600

    More input tests

--- test/Input.test.js
+++ test/Input.test.js
@@ -2,22 +2,19 @@ import { shallow, mount, render } from 'enzyme'
 import Input from '../lib/Input'
 import React from 'react'
 import { expect } from 'chai'
-import { Component } from 'react'
 
 describe('Input: ', () => {
-  //
+
   // beforeEach( () => {
-  //   const wrapper = shallow(<Input />)
-  //   console.log(debug())
+  //   returconst wrapper = shallow(<Input />)
   // })
 
   it('should instantiate with a nav bar', () => {
     const wrapper = shallow(<Input />)
-    // console.log(wrapper.debug())
     expect(wrapper.is('nav')).to.equal(true)
   })
 
-  it('should instantiate with a input, submit button, and drop down menu', () => {
+  it('should instantiate with an input, submit button, and drop down menu', () => {
     const wrapper = shallow(<Input />)
 
     expect(wrapper.containsMatchingElement([<input></input>])).to.equal(true)
@@ -25,4 +22,37 @@ describe('Input: ', () => {
     expect(wrapper.containsMatchingElement([<select></select>])).to.equal(true)
   })
 
+  it('should instantiate with an input, submit button, and drop down menu', () => {
+    const wrapper = shallow(<Input />)
+
+    expect(wrapper.containsMatchingElement([<input></input>])).to.equal(true)
+    expect(wrapper.containsMatchingElement([<button></button>])).to.equal(true)
+    expect(wrapper.containsMatchingElement([<select></select>])).to.equal(true)
+  })
+
+  it('should instantiate w/ an empty location state', () => {
+    const wrapper = shallow(<Input />)
+
+    expect(wrapper.state('location')).to.equal('')
+  })
+
+  it('should instantiate w/ an empty apilocationSuggestion state', () => {
+    const wrapper = shallow(<Input />)
+
+    expect(wrapper.state('apiLocationSuggestion')).to.deep.equal({})
+  })
+
+  it('should instantiate w/ an EN as the default language in state', () => {
+    const wrapper = shallow(<Input />)
+
+    expect(wrapper.state('language')).to.deep.equal('EN')
+  })
+
+  it('should instantiate w/ an EN as the default language in state', () => {
+    const wrapper = shallow(<Input />)
+
+    expect(wrapper.state('language')).to.deep.equal('EN')
+  })
+
+  
 })
